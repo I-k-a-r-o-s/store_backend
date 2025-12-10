f9fb869011b7d49fb52211e6ecf0e1c71a329a05
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-10 16:28:04 +0530

    finishwed building product model

---
+++ src/models/product.model.js
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = model("Product", productSchema);
+
+export default Product;
