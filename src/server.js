1a61396c04ac451193970c64126dafd9f0cbe990
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-10 16:28:56 +0530

    added database connection

--- src/server.js
+++ src/server.js
@@ -2,7 +2,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import connectDatabase from "./config/database.js";
 
 const server = express();
 
 server.use(express.json());
+
+const PORT = process.env.PORT;
+
+const startServer = async () => {
+  try {
+    await connectDatabase();
+    server.listen(PORT, () => {
+      console.log("Server Started on Port:", PORT);
+    });
+  } catch (error) {
+    console.log("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
