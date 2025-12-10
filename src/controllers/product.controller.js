45db9a50bb02c48c77a51267ce794a44075e919d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-10 17:33:52 +0530

    added a little verification for checking product existance

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -29,6 +29,12 @@ export const createProduct = async (req, res) => {
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
+    if (products.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No products found",
+      });
+    }
     res.status(200).json({
       success: true,
       products,
