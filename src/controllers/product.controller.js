bff4f458667699f234a44a55c6eaccc1542a9e6d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-10 17:30:47 +0530

    fixed a typo

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -29,7 +29,7 @@ export const createProduct = async (req, res) => {
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).jsson({
+    res.status(200).json({
       success: true,
       products,
     });
