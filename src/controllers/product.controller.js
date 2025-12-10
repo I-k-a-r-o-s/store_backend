b5f607488ee838e62bcaaff6198537889c8368a0
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-12-10 17:09:47 +0530

    finished creating controllers

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,15 +1,99 @@
-import Product from "../models/product.model.js"
+import Product from "../models/product.model.js";
 
 //create product
 export const createProduct = async (req, res) => {
-    try {
-        const {name,price,image}=req.body
-        if(!name || !price || !image){
-            return res.status(400).json({message:"All fields are required"})
-        }
-        const product=await Product.create({name,price,image})
-        res.status(201).json({message:"Product created successfully",product})
-    } catch (error) {
-        res.status(500).json({message:"Server Error",error:error.message})
+  try {
+    const { name, price, image } = req.body;
+    if (!name || !price || !image) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
-}
+    const product = await Product.create({ name, price, image });
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
+
+//get all products
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).jsson({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
+
+//update product
+export const updateProduct = async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide data to update",
+      });
+    }
+
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
+
+//delete product
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message,
+    });
+  }
+};
